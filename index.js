06cc9b88e8c3853e8aba06d807765b0153a3dcc6
ZB <ztenerowicz@egnyte.com>
2015-06-01 11:32:00 +0200

    error message for missing id

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 var router = express.Router();
 
-router.use('/definition.json', function (req, res) {
+router.get('/definition.json', function (req, res) {
     res.sendFile(path.resolve(__dirname, './definition.json'))
 })
 router.use('/staticapp', express.static(__dirname + "/public"))
@@ -30,8 +30,12 @@ router.post('/service', function (req, res) {
 })
 
 router.get('/service/:id', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.status(200).send(JSON.stringify(sessions[req.params.id], null, 4));
+    if (!sessions[req.params.id]) {
+        res.status(500).end("Unrecognized redirect id: %s", req.params.id);
+    } else {
+        res.set('Content-Type', 'application/json');
+        res.status(200).send(JSON.stringify(sessions[req.params.id], null, 4));
+    }
 })
 router.get('/service', function (req, res) {
     res.status(400).send('You were supposed to make a POST request')
